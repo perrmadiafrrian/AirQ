1dbd96cfca4824d35faca6a4949e7971c62c8bde
R <permadi.afrian@gmail.com>
2020-05-17 21:11:43 +0700

    commit branch

---
+++ precache-manifest.31c429b864c7986adeae900e5328dc1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e452c3cefba5fd4bafb995033bec2c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901279bd4f8ba03fa628",
+    "url": "/static/css/main.5dfe907a.chunk.css"
+  },
+  {
+    "revision": "bfb8d18ab4fb9d8c7755",
+    "url": "/static/js/2.fb6c1d8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb6c1d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901279bd4f8ba03fa628",
+    "url": "/static/js/main.22bce1f6.chunk.js"
+  },
+  {
+    "revision": "5003d5c2149426c546c5",
+    "url": "/static/js/runtime-main.f8526495.js"
+  },
+  {
+    "revision": "85b02b56eb1db1573e24dd90b7ee31f5",
+    "url": "/static/media/miyg.85b02b56.png"
+  }
+]);
