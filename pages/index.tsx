8b2f1d4797df03f71db01d3805d9a0b60d8ccf89
Rian <permadi.afrian@geniebook.com>
2021-11-27 14:35:55 +0700

    patch: adding permission checking

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,47 @@ type Data = {
   lon: number | null;
 };
 
+enum Permissions {
+  GRANTED = "granted",
+  DENIED = "denied",
+  PROMPT = "prompt",
+}
+
 const Index: NextPage = () => {
-  const [geolocated, setGeolocated] = useState<Boolean>(false);
+  const [permission, setPermission] = useState<Permissions>(Permissions.PROMPT);
   const [geolocation, setGeolocation] = useState<Data>({
     lat: null,
     lon: null,
   });
 
+  const checkResult = (permission_state: string) => {
+    switch (permission_state) {
+      case Permissions.PROMPT:
+        setPermission(Permissions.PROMPT);
+        break;
+      case Permissions.GRANTED:
+        setPermission(Permissions.GRANTED);
+        break;
+      case Permissions.DENIED:
+      default:
+        setPermission(Permissions.DENIED);
+        break;
+    }
+  };
+
   useEffect(() => {
+    //Getting permissions
+    navigator.permissions
+      .query({ name: "geolocation" })
+      .then(function (result) {
+        checkResult(result.state);
+
+        result.onchange = (ev) => {
+          const res = ev.currentTarget as PermissionStatus;
+          checkResult(res.state);
+        };
+      });
+
     //Getting geolocation of client's browser
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((data: GeolocationPosition) => {
@@ -23,21 +56,28 @@ const Index: NextPage = () => {
           lat: data.coords.latitude,
           lon: data.coords.longitude,
         });
-        setGeolocated(true);
       });
     }
   }, []);
 
-  if (geolocated)
-    return (
-      <WeatherPage
-        location={"Colomadu"}
-        temperature={"22"}
-        date={"November, 25th"}
-        year={"2021"}
-      />
-    );
-  else return <InfoPage message="Getting client's information" />;
+  switch (permission) {
+    case Permissions.GRANTED:
+      return (
+        <WeatherPage
+          location={"Colomadu"}
+          temperature={"22"}
+          date={"November, 25th"}
+          year={"2021"}
+        />
+      );
+    case Permissions.PROMPT:
+      return <InfoPage message="Getting client's information" />;
+    case Permissions.DENIED:
+    default:
+      return (
+        <InfoPage message="Client's location permission denied. Please allow location permission on your device" />
+      );
+  }
 };
 
 export default Index;
