77370a94b6d41993d73ab10167e14f3e98ad778b
Rian <permadi.afrian@geniebook.com>
2021-11-27 15:51:21 +0700

    patch: get client location and temperature

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import WeatherPage from "../components/WeatherPage";
 import InfoPage from "../components/InfoPage";
+import axios from "axios";
 
 type Data = {
   lat: number | null;
@@ -35,6 +36,43 @@ const months: string[] = [
   "December",
 ];
 
+type ResData = {
+  coord: Data;
+  weather: WeatherData[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sealevel: number;
+    grnd_level: number;
+  };
+  visibility: number;
+  wind: { speed: number; deg: number; gust: number };
+  rain: {
+    "1h": number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: { country: string; sunrise: number; sunset: number };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+};
+
+type WeatherData = {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+};
+
 const Index: NextPage = () => {
   const [permission, setPermission] = useState<Permissions>(Permissions.PROMPT);
   const [geolocation, setGeolocation] = useState<Data>({
@@ -46,6 +84,8 @@ const Index: NextPage = () => {
     year: null,
     month: null,
   });
+  const [location, setLocation] = useState<string>("");
+  const [temperature, setTemperature] = useState<string | number>("");
 
   // Get number ordinal
   const getOrdinal = (n: number): string => {
@@ -111,14 +151,25 @@ const Index: NextPage = () => {
 
   useEffect(() => {
     //Getting weather information
+    if (geolocation.lat !== null && geolocation.lon !== null)
+      axios
+        .get(`/api/weather?lat=${geolocation.lat}&lon=${geolocation.lon}`)
+        .then(({ data }) => {
+          const res_data: ResData = data.data;
+          setLocation(res_data.name);
+          setTemperature(res_data.main.temp);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
   }, [geolocation]);
 
   switch (permission) {
     case Permissions.GRANTED:
       return (
         <WeatherPage
-          location={"Colomadu"}
-          temperature={"22"}
+          location={location}
+          temperature={`${temperature}`}
           date={`${today.month}, ${today.date}`}
           year={today.year}
         />
